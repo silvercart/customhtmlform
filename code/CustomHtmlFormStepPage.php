4ea03955494600a24b72913c2a552b523ff96a18
Sascha Koehler <skoehler@pixeltricks.de>
2010-10-01 16:46:29 +0200

    Vor/Zurueck-Buttons funktionieren Daten werden in Session gespeichert Sessiondaten werden in Formulare geschrieben bei Vor/Zurueck

--- code/CustomHtmlFormStepPage.php
+++ code/CustomHtmlFormStepPage.php
@@ -42,7 +42,7 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
      * 
      * @var integer
      */
-    protected $nrOfSteps;
+    protected $nrOfSteps = -1;
     
     /**
      * Enthaelt die Nummer des aktuellen Schritts.
@@ -68,7 +68,34 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
         return $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['currentStep'];
     }
 
-        /**
+    /**
+     * Liefert die als abgeschlossen markierten Schritte als numerisches Array
+     * zurueck.
+     *
+     * @return array
+     */
+    public function getCompletedSteps() {
+        return $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['completedSteps'];
+    }
+
+    /**
+     * Speichert einen Schritt als abgeschlossen.
+     *
+     * @param integer $stepNr: Nummer des aktuellen Schritts. Wenn weggelassen,
+     *                     wird der aktuelle Schritt automatisch eingesetzt.
+     */
+    public function addCompletedStep($stepNr = null) {
+
+        if ($stepNr === null) {
+            $stepNr = $this->getCurrentStep();
+        }
+        
+        if (!in_array($this->getCurrentStep(), $this->getCompletedSteps())) {
+            $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['completedSteps'][] = $stepNr;
+        }
+    }
+
+    /**
      * Ruft die gleichnamige Methode der Elternseite auf und erstellt den
      * passenden Parameter.
      */
@@ -76,6 +103,143 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
         $formIdentifier = $this->basename.$this->getCurrentStep();
 
         return parent::InsertCustomHtmlForm($formIdentifier);
+    }
+
+    /**
+     * Speichert die Formulardaten des aktuellen Schritts.
+     *
+     * @param array $formData
+     */
+    public function setStepData($formData, $stepNr = null) {
+
+        if ($stepNr === null) {
+            $stepNr = $this->getCurrentStep();
+        }
+
+        $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['steps'][$stepNr] = $formData;
+    }
+
+    /**
+     * Liefert die Daten des aktuellen Schritts als assoziatives Array. Sind
+     * keine Daten vorhanden, wird false zurueckgegeben.
+     *
+     * @return array | boolean false
+     */
+    public function getStepData($stepnr = null) {
+
+        if ($stepNr === null) {
+            $stepNr = $this->getCurrentStep();
+        }
+
+        if (isset($_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['steps'][$stepNr])) {
+            return $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['steps'][$stepNr];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Befuellt die Felder des mitgelieferten Formulars mit den ggfs. in der
+     * Session gespeicherten Werten.
+     * 
+     * @param array $fields 
+     */
+    public function fillFormFields($fields) {
+
+        $formSessionData    = $this->getStepData();
+        $fieldIdx           = 0;
+
+        foreach ($fields as $fieldName => $fieldData) {
+
+            if (isset($formSessionData[$fieldName])) {
+                $fields[$fieldName]['value'] = $formSessionData[$fieldName];
+            }
+        }
+    }
+
+    /**
+     * Gibt die Nummer des vorhergehenden Schritts zurueck.
+     *
+     * @return integer
+     */
+    public function getPreviousStep() {
+        $currentStep = $this->getCurrentStep();
+
+        return $currentStep - 1;
+    }
+
+    /**
+     * Gibt die Nummer des folgenden Schritts zurueck.
+     *
+     * @return integer
+     */
+    public function getNextStep() {
+        $currentStep = $this->getCurrentStep();
+
+        return $currentStep + 1;
+    }
+
+    /**
+     * Setzt die Nummer des aktuellen Schritts
+     *
+     * @param integer $stepNr
+     */
+    public function setCurrentStep($stepNr) {
+        $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['currentStep'] = $stepNr;
+    }
+
+    /**
+     * Gibt den Link zur vorhergehenden Seite zurueck.
+     *
+     * @return string | boolean false
+     */
+    public function CustomHtmlFormStepLinkPrev() {
+        $link = false;
+
+        if ($this->getPreviousStep() > 0 &&
+            in_array($this->getPreviousStep(), $this->getCompletedSteps()) ) {
+
+            $link = $this->Link('PreviousStep');
+        }
+
+        return $link;
+    }
+
+    /**
+     * Gibt den Link zur folgenden Seite zurueck.
+     *
+     * @return string | boolean false
+     */
+    public function CustomHtmlFormStepLinkNext() {
+        $link = false;
+
+        if (in_array($this->getNextStep(), $this->getCompletedSteps())) {
+            $link = $this->Link('NextStep');
+        }
+
+        return $link;
+    }
+
+    /**
+     * Erhoeht den aktuellen Schritt
+     */
+    public function NextStep() {
+        if (in_array($this->getNextStep(), $this->getCompletedSteps())) {
+            $this->setCurrentStep($this->getNextStep());
+        }
+        Director::redirect($this->Link(), 302);
+    }
+
+    /**
+     * Vermindert den aktuellen Schritt
+     */
+    public function PreviousStep() {
+        if ($this->getPreviousStep() > 0 &&
+            in_array($this->getPreviousStep(), $this->getCompletedSteps()) ) {
+
+            $this->setCurrentStep($this->getPreviousStep());
+        }
+        Director::redirect($this->Link(), 302);
     }
 
     /**
@@ -96,7 +260,7 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
      *     {PageClass}{PageID}
      *       currentStep    => Int          Default: 1
      *       completedSteps => array()      Default: empty
-     *
+     *       steps          => array()      Default: empty
      */
     protected function initialiseSessionData() {
         if (!isset($_SESSION['CustomHtmlFormStep'])) {
@@ -110,6 +274,9 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
         }
         if (!isset($_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['completedSteps'])) {
             $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['completedSteps'] = array();
+        }
+        if (!isset($_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['steps'])) {
+            $_SESSION['CustomHtmlFormStep'][$this->ClassName.$this->ID]['steps'] = array();
         }
     }
 
@@ -126,6 +293,11 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
      * @return integer
      */
     protected function getNumberOfSteps() {
+
+        if ($this->nrOfSteps > -1) {
+            return $this->nrOfSteps;
+        }
+
         $themePath      = THEMES_DIR.'/'.SSViewer::current_theme().'/templates/Layout/';
         $increaseStep   = true;
         $stepIdx        = 1;
