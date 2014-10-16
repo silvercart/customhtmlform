7ce7fc0bc08066c45b1a76f2c6b7bb2e2037517b
Sebastian Diel <dev@sdiel.de>
2014-10-16 15:32:06 +0200

    BUGFIX: Fixed some 3.1 compatibillity issues.

--- code/pages/CustomHtmlFormStepPage.php
+++ code/pages/CustomHtmlFormStepPage.php
@@ -540,9 +540,9 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
             header('Location: ' . $this->Link(), true, 302);
             exit();
         } else {
-            $redirected_to = Director::redirected_to();
+            $redirected_to = $this->Controller()->redirectedTo();
             if (empty($redirected_to)) {
-                Director::redirect($this->Link(), 302);
+                $this->Controller()->redirect($this->Link(), 302);
             }
         }
     }
@@ -568,9 +568,9 @@ class CustomHtmlFormStepPage_Controller extends Page_Controller {
             header('Location: ' . $this->Link(), true, 302);
             exit();
         } else {
-            $redirected_to = Director::redirected_to();
+            $redirected_to = $this->Controller()->redirectedTo();
             if (empty($redirected_to)) {
-                Director::redirect($this->Link(), 302);
+                $this->Controller()->redirect($this->Link(), 302);
             }
         }
     }
